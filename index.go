bd24f362f0c10117c1b1b982811d17a0e7155c03
Ravan <velmurugan1211r@gmail.com>
2024-08-12 09:14:47 +0530

    first commit

--- index.go
+++ index.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var name = "John Doe"
+
+// Declare the structure
+type Employee struct {
+	fName, lName string
+	age          int
+}
 
 func main() {
 	var hello string = "Hiii"
@@ -29,4 +37,44 @@ func main() {
 	fmt.Println(4 / 2)
 	fmt.Println(2 * 2)
 	fmt.Println(9 % 2)
+
+	// Named Structs
+	fmt.Println("Struct in Go...!")
+	emp1 := Employee{
+		fName: "Vel",
+		lName: "Murugan",
+		age:   23,
+	}
+	emp2 := Employee{
+		"Varan",
+		"Raghu",
+		22,
+	}
+	fmt.Println(emp1)
+	fmt.Println(emp2)
+
+	// Anonymous structs
+	userData := struct{ fname, lname string }{
+		"Vel", "murugan",
+	}
+	fmt.Println(userData)
+
+	// Function
+	total := calcBill(12, 78)
+	fmt.Println(total)
+
+	// Method
+	emp1.getUserData()
+	emp2.getUserData()
+
+}
+
+// Function in Go
+func calcBill(a, b int) int {
+	return a + b
+}
+
+// Method
+func (e Employee) getUserData() {
+	println(e.fName, e.lName, "Age: ", e.age)
 }
